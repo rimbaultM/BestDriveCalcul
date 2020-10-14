3089d9579f0d8099b87a60f4a07de5fbc16321d4
mathieu rimbault <rimbaultandco@gmail.com>
2020-10-14 16:17:07 +0200

    home+choices

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Bestdrive = User.new(email: "Bestdrive@gmail.com", password: "bestdrive")
+Bestdrive.save!
+puts "Users created!"
