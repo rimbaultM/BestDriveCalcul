ce6eaa9cf72536f4665483b75b93b3f5156aa0b3
mathieu rimbault <rimbaultandco@gmail.com>
2020-11-01 22:09:02 +0100

    michelin v1.2

--- app/controllers/answer2_controller.rb
+++ app/controllers/answer2_controller.rb
@@ -1,6 +1,26 @@
 class Answer2Controller < ApplicationController
    def index
     @etroite = Etroite.first
+
+    input1 = (@etroite.fam + ((@etroite.janteexterieur )*2)-((@etroite.voilecreux )*2)-((@etroite.jantelargeur) * 25.4)-((@etroite.voileepaisseur)*2)).round(-1)
+    input2 = (@etroite.fam + ((@etroite.jantesepaisseur )*2)+((@etroite.janteexterieur)*2)-((@etroite.voilecreux )*2)-((@etroite.jantelargeur) * 25.4)).round(-1)
+    input3 = (@etroite.fam + ((@etroite.janteexterieur )*2)+((@etroite.cic)*2)-((@etroite.voilecreux )*2)-((@etroite.jantelargeur) * 25.4)).round(-1)
+    input4 = (@etroite.fam + ((@etroite.voileepaisseur )*2)+((@etroite.jantesepaisseur)*2)+((@etroite.janteexterieur)*2)+((@etroite.cic)*2)-((@etroite.voilecreux )*2)-((@etroite.jantelargeur) * 25.4)).round(-1)
+    input5 = (@etroite.fam + ((@etroite.jantelargeur) * 25.4)-((@etroite.voilecreux )*2)-((@etroite.janteexterieur)*2)-((@etroite.jantesepaisseur )*2)-((@etroite.voileepaisseur)*2)).round(-1)
+    input6 = (@etroite.fam + @etroite.cic+((@etroite.jantelargeur) * 25.4)-((@etroite.voilecreux )*2)-((@etroite.janteexterieur)*2)-((@etroite.jantesepaisseur)*2)).round(-1)
+    input7 = (@etroite.fam + ((@etroite.jantelargeur) * 25.4)-((@etroite.voilecreux )*2)-((@etroite.janteexterieur )*2)).round(-1)
+    input8 = (@etroite.fam + ((@etroite.voileepaisseur )*2)+((@etroite.cic)*2)+((@etroite.jantelargeur) * 25.4)-((@etroite.voilecreux )*2)-((@etroite.janteexterieur )*2)).round(-1)
+    input9 = (@etroite.fam + ((@etroite.voilecreux )*2)+((@etroite.janteexterieur)*2)-((@etroite.jantelargeur) * 25.4)-((@etroite.voileepaisseur)*2)).round(-1)
+    input10 = (@etroite.fam + ((@etroite.voilecreux )*2)+((@etroite.janteexterieur)*2)+((@etroite.cic)*2)-((@etroite.jantelargeur) * 25.4)).round(-1)
+    input11 = (@etroite.fam + ((@etroite.voilecreux )*2)+((@etroite.jantesepaisseur )*2)+((@etroite.janteexterieur)*2)-((@etroite.jantelargeur)* 25.4)).round(-1)
+    input12 = (@etroite.fam + ((@etroite.voilecreux )*2)+((@etroite.voileepaisseur)*2)+((@etroite.jantesepaisseur )*2)+((@etroite.janteexterieur)*2)+((@etroite.cic)*2)-((@etroite.jantelargeur)* 25.4)).round(-1)
+    input13 = (@etroite.fam + ((@etroite.voilecreux )*2)+((@etroite.jantelargeur) * 25.4)-((@etroite.janteexterieur)*2)-((@etroite.jantesepaisseur )*2)-((@etroite.voileepaisseur)*2)).round(-1)
+    input14 = (@etroite.fam + ((@etroite.voilecreux )*2)+((@etroite.cic)*2)+((@etroite.jantelargeur) * 25.4)-((@etroite.janteexterieur)*2)-((@etroite.jantesepaisseur )*2)).round(-1)
+    input15 = (@etroite.fam + ((@etroite.voilecreux )*2)+((@etroite.jantelargeur) * 25.4)-((@etroite.janteexterieur)*2)).round(-1)
+    input16 = (@etroite.fam + ((@etroite.voilecreux )*2)+((@etroite.voileepaisseur)*2)+((@etroite.cic)*2)+((@etroite.jantelargeur) * 25.4)-((@etroite.janteexterieur)*2)).round(-1)
+    calculetroit = [input1,input2,input3,input4,input5,input6,input7,input8,input9, input10, input11, input12, input13, input14, input15, input16]
+    @sortinputetroit  = calculetroit.sort
+
   end
 
   private
--- app/controllers/answer_controller.rb
+++ app/controllers/answer_controller.rb
@@ -2,6 +2,18 @@ class AnswerController < ApplicationController
 
   def index
     @standard = Standard.first
+
+    input1 = (@standard.fam + ((@standard.janteexterieur)*2)-((@standard.voilecreux )*2)- ((@standard.jantelargeur) * 25.4)).round(-1)
+    input2 = (@standard.fam + ((@standard.voileepaisseur)*2)+((@standard.jantesepaisseur )*2)+((@standard.janteexterieur)*2)-((@standard.voilecreux)*2)-((@standard.jantelargeur) * 25.4)).round(-1)
+    input3 = (@standard.fam + ((@standard.jantelargeur)* 25.4)-((@standard.voilecreux )*2)-((@standard.janteexterieur)*2)-((@standard.jantesepaisseur)*2)).round(-1)
+    input4 = (@standard.fam + ((@standard.voileepaisseur)*2)+((@standard.jantelargeur)* 25.4)-((@standard.voilecreux )*2)-((@standard.janteexterieur)*2)).round(-1)
+    input5 = (@standard.fam + ((@standard.voilecreux )*2)+((@standard.janteexterieur)*2)-((@standard.jantelargeur) * 25.4)).round(-1)
+    input6 = (@standard.fam + ((@standard.voilecreux )*2)+((@standard.voileepaisseur)*2)+((@standard.jantesepaisseur )*2)+((@standard.janteexterieur)*2)-((@standard.jantelargeur) * 25.4)).round(-1)
+    input7 = (@standard.fam + ((@standard.voilecreux )*2)+((@standard.jantelargeur) * 25.4)-((@standard.janteexterieur)*2)-((@standard.jantesepaisseur)*2)).round(-1)
+    input8 = (@standard.fam + ((@standard.voilecreux )*2)+((@standard.jantesepaisseur)*2)+((@standard.jantelargeur) * 25.4)-((@standard.janteexterieur)*2)).round(-1)
+    calculstandard = [input1,input2,input3,input4,input5,input6,input7,input8]
+    @sortinputstandard = calculstandard.sort
+
   end
 
   private
